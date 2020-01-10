b85af0caee4d4da28d826e56bc2234c6f9fcf3cc
ShwetaLohar03 <59738479+ShwetaLohar03@users.noreply.github.com>
2020-01-10 07:31:30 -0800

    Update new.c

--- new.c
+++ new.c
@@ -15,6 +15,7 @@ struct student
 int main()
 {
     int struct student s = {101,"Shweta",70.00,70.00};
+    int struct student s1 = {102,"Shruti",75.20,75.20};
 
     printf("Size of Structure is %d\n",sizeof(struct student));
     
@@ -22,6 +23,7 @@ int main()
     
     printf("----------Student Data----------\n");
     printf("Roll no\t%d\n Name\t%s\n Marks\t%f\n Total\t%lf\n",s.rn,s.name,s.marks,s.total);
+    printf("Roll no\t%d\n Name\t%s\n Marks\t%f\n Total\t%lf\n",s1.rn,s1.name,s1.marks,s1.total);
 
     
     return 0;
